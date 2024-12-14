bbbd404c32b34cc44e295b46c85ecc0c4ef9f45a
AakashB-1996 <bolisettyaakash@gmail.com>
2024-12-13 21:45:57 -0600

    Create consecutive_available_seat.sql

---
+++ Easy/consecutive_available_seat.sql
@@ -0,0 +1,52 @@
+# Write your MySQL query statement below
+select distinct l.seat_id as seat_id from Cinema l, Cinema r
+where (l.seat_id = r.seat_id -1 or l.seat_id = r.seat_id+1) ##accounts for all the seats including the last seat
+and l.free = r.free  and l.free = 1
+order by 1
+
+  """
+Table: Cinema
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| seat_id     | int  |
+| free        | bool |
++-------------+------+
+seat_id is an auto-increment column for this table.
+Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
+ 
+
+Find all the consecutive available seats in the cinema.
+
+Return the result table ordered by seat_id in ascending order.
+
+The test cases are generated so that more than two seats are consecutively available.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Cinema table:
++---------+------+
+| seat_id | free |
++---------+------+
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |
++---------+------+
+Output: 
++---------+
+| seat_id |
++---------+
+| 3       |
+| 4       |
+| 5       |
++---------+
+
+"""
